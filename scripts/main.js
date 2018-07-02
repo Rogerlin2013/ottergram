eeedb16a15a6502e58c9423bbc9f7c55c72bfdda
linyongzhi <yongzhi.lin@uama.com.cn>
2018-07-02 16:14:01 +0800

    添加一些交互动画，改进体验

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role = "trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY_CODE = 27;
 
 function imageFromThumb(thumbnail) {
@@ -59,6 +61,12 @@ function hideDetails() {
 function showDetails() {
     'use strict'
     document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    frame.classList.add(TINY_EFFECT_CLASS);
+
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
 }
 
 function addKeyPressHandler() {
